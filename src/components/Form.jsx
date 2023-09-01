adc9cfbe81d264674feb54931c9921b5e0d4d34c
Samuel Sharivker <114585707+samuels0052@users.noreply.github.com>
2023-08-31 22:39:38 -0400

    updated form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Form() {
+    const [input, setInput] = useState('');
     return (
         <div className="container" id="contact-form-body">
             <h1 class="pb-5">Contact Form</h1>
@@ -11,14 +12,14 @@ function Form() {
                 </div>
                 <div className="mb-3">
                     <label htmlFor="emailForm" className="form-label">Email</label>
-                    <input name="email" type="email" className="form-control" id="emailForm" placeholder="name@example.com" required/>
+                    <input value={input} onChange={(e) => setInput(e.target.value)} name="email" type="email" className="form-control" id="emailForm" placeholder="name@example.com" required/>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="messageForm" className="form-label">Message</label>
                     <textarea name="message" className="form-control" id="messageForm" rows="6" required></textarea>
                 </div>
                 <input type="hidden" name="_next" value="https://samuelsharivker.com/thanks" />
-                <input type="hidden" name="_subject" value="New submission!" />
+                <input type="hidden" name="_subject" value={setInput} />
                 <button type="submit" className="btn btn-light">Submit</button>
             </form>
         </div>
