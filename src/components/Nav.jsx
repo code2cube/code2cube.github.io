93cb333cd07b517c73fd867d271139104e21d70e
Samuel Sharivker <114585707+samuels0052@users.noreply.github.com>
2023-09-07 18:21:51 -0400

    added linkedin

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -9,26 +9,27 @@ function Nav() {
         });
     };
 
+    const isActiveLink = (pathname) => {
+        return window.location.pathname === pathname ? 'active' : '';
+    };
+
     return (
         <div class="container-fluid sticky-top" id="navbar-wrapper">
             <nav class="navbar navbar-expand-md">
                 <div class="container-fluid">
-                    <Link class="navbar-brand" to="/" onClick={scrollToTop}><span id="brand_span">live2cube</span></Link>
+                    <Link className={`navbar-brand ${isActiveLink('/')}`} to="/" onClick={scrollToTop}><span id="brand_span">live2cube</span></Link>
                     <button class="navbar-toggler poop" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     </button>
                     <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
                         <ul class="navbar-nav">
                             <li class="nav-item">
-                                <Link class="nav-link" to="/about" onClick={scrollToTop}><i class="bi bi-info-square-fill"></i><span> About</span></Link>
+                                <Link className={`nav-link ${isActiveLink('/about')}`} to="/about" onClick={scrollToTop}><i class="bi bi-info-square-fill"></i><span> About</span></Link>
                             </li>
                             <li class="nav-item">
-                                <Link class="nav-link" to="/contact" onClick={scrollToTop}><i class="bi bi-envelope-fill"></i><span> Contact</span></Link>
+                                <Link className={`nav-link ${isActiveLink('/contact')}`} to="/contact" onClick={scrollToTop}><i class="bi bi-envelope-fill"></i><span> Contact</span></Link>
                             </li>
                             <li class="nav-item">
-                                <Link class="nav-link" to="https://github.com/code2cube/projects" target="_blank" rel="noreferrer"><i class="bi bi-file-earmark-code-fill"></i><span> Projects</span></Link>
-                            </li>
-                            <li class="nav-item">
-                                <Link class="nav-link" to="https://www.linkedin.com/in/samuel-sharivker-74158028b/" target="_blank" rel="noreferrer"><i class="bi bi-file-person-fill"></i><span> Resume</span></Link>
+                                <Link class="nav-link" to="https://www.linkedin.com/in/samuel-sharivker-74158028b/" target="_blank" rel="noreferrer"><i class="bi bi-linkedin"></i><span> Linkedin</span></Link>
                             </li>
                         </ul>
                     </div>
