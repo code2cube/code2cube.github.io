a0226b9066078d0399238ce1864836e812bf335b
Samuel Sharivker <114585707+samuels0052@users.noreply.github.com>
2023-09-08 14:57:58 -0400

    updated error page

--- src/pages/NoPage.jsx
+++ src/pages/NoPage.jsx
@@ -1,12 +1,29 @@
-function NoPage() {
+import '../App.css';
+import { Link } from 'react-router-dom';
+import Nav from '../components/Nav';
+import Footer from '../components/Footer';
+
+function ThanksPage() {
 
     document.title = "Error: 404 | Samuel Sharivker"
 
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    };
+
     return (
-        <div>
-            <h1>Error 404: This Page Does Not Exist!</h1>
+        <div id="App">
+            <Nav />
+            <div className="container-fluid text-center" id="no-page">
+                <h1 class="display-1 home-text">Error: 404! This Page Does Not Exist!</h1>
+                <h6 class="display-6 home-text"><Link id="thanks-return" to="/" onClick={scrollToTop}>Return Home</Link></h6>
+            </div>
+            <Footer />
         </div>
-    )
+    );
 }
 
-export default NoPage;
+export default ThanksPage;
