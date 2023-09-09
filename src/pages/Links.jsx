dd51c48f8137a09d587413d585df0ee204dcda21
Samuel Sharivker <114585707+samuels0052@users.noreply.github.com>
2023-09-08 20:20:29 -0400

    added mail to copy

--- src/pages/Links.jsx
+++ src/pages/Links.jsx
@@ -1,3 +1,5 @@
+import React, {useState} from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 import Nav from '../components/Nav';
 import Footer from '../components/Footer';
 
@@ -5,13 +7,19 @@ function Links() {
 
     document.title = "Links | Samuel Sharivker"
 
+    const myMail = "sam@samuelsharivker.com";
+    const [copied, setCopied] = useState(false);
+
     return (
         <div id="App">
             <Nav />
             <div className="container-fluid text-center" id="links-page">
                 <div className="row link-container">
                     <div className="col-md-12 link-wrapper">
-                        <p>Mail</p>
+                        <CopyToClipboard text={myMail} onCopy={() => setCopied(true)}>
+                            <p>Mail</p>
+                        </CopyToClipboard>
+                        {copied ? alert("Mail has beeen Copied!"): null}
                     </div>
                     <div className="col-md-12 link-wrapper">
                         <a href="https://github.com/code2cube" target="_blank" rel="noreferrer"><p>Github</p></a>
